0dacd6583095d913b3e11e742437152e76228ed2
aswath0001 <rsaswaths38@gmail.com>
2025-03-05 10:30:47 +0530

    Your commit message

--- src/assets/components/pages/employeePage.jsx
+++ src/assets/components/pages/employeePage.jsx
@@ -105,7 +105,7 @@ const EmployeesPage = () => {
               <button
                 onClick={() => navigate("/leads")}
                 className={`px-4 py-2 rounded-lg text-gray-700 hover:text-blue-500 transition-all ${
-                  location.pathname === "/leads" ? "bg-gray-200" : ""
+                  location.pathname === "/leads" ? "bg-gray-300" : ""
                 }`}
               >
                 Leads
